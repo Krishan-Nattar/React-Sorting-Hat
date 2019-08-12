8196511a0c7cf549badc49aa9d6c3de8e20f479c
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-12 13:31:58 -0700

    refactor

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import WelcomePage from './components/WelcomePage';
 import {Route, Link} from 'react-router-dom';
@@ -9,10 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* App Component */}
-        {/* <WelcomePage /> */}
-       {/* Start with Welcome Page */}
-       {/* Routes you to Question Page */}
+
        <Route exact exact path="/" component={WelcomePage} />
        <Route exact path="/questions" component={Questions} />
       </div>
