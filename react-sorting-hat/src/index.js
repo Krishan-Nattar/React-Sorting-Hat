db4527d51c2ef33ce970b1dff2f9274824b59790
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-12 17:46:33 -0700

    Fully functional!

--- react-sorting-hat/src/index.js
+++ react-sorting-hat/src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import Gryffindor from './components/Gryffindor'
-import HufflePuff from './components/Hufflepuff';
-import Ravenclaw from './components/Ravenclaw';
-import Slytherin from './components/Slytherin';
 
 ReactDOM.render(
   <Router>
-    {/* <App /> */}
-    <Gryffindor />
-    <HufflePuff />
-    <Ravenclaw />
-    <Slytherin />
+    <App />
   </Router>,
   document.getElementById("root")
 );
