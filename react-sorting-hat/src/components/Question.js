bd442116ce59cd3ecc540e516dba8b47217b03ae
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-02 14:04:24 -0700

    Refactor

--- react-sorting-hat/src/components/Question.js
+++ react-sorting-hat/src/components/Question.js
@@ -1,33 +1,38 @@
-import React from 'react';
-import { Button, Card, Image } from 'semantic-ui-react'
+import React from "react";
+import { Button, Card, Image } from "semantic-ui-react";
 
 class Question extends React.Component {
-    state = {  }
-    render(props) { 
-        return ( 
-        <div>
-             <Card>
-      <Card.Content>
-        <Image size='small' src='./images/sortinghat-black.svg' />
-        <Card.Description>
-        {this.props.question[0]}
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button basic color='green' onClick={()=>this.props.handleAnswer(this.props.question[3])}>
-            {this.props.question[1]}
-          </Button>
-          <Button basic color='purple' onClick={()=>this.props.handleAnswer(this.props.question[4])}>
-            {this.props.question[2]}
-          </Button>
-        </div>
-      </Card.Content>
-    </Card>
-            
-            </div> 
-            );
-    }
+  state = {};
+  render(props) {
+    return (
+      <div>
+        <Card>
+          <Card.Content>
+            <Image size="small" src="./images/sortinghat-black.svg" />
+            <Card.Description>{this.props.question[0]}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className="ui two buttons">
+              <Button
+                basic
+                color="green"
+                onClick={() => this.props.handleAnswer(this.props.question[3])}
+              >
+                {this.props.question[1]}
+              </Button>
+              <Button
+                basic
+                color="purple"
+                onClick={() => this.props.handleAnswer(this.props.question[4])}
+              >
+                {this.props.question[2]}
+              </Button>
+            </div>
+          </Card.Content>
+        </Card>
+      </div>
+    );
+  }
 }
- 
+
 export default Question;
